4cef0301893661ade5fd1f68c2b24581acf480ee
Giovanni Guerreiro <85262106+giovanni6776@users.noreply.github.com>
2023-01-03 14:02:29 -0300

    Add files via upload

---
+++ experiments/HPC_scripts/train_gpua10.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+### General options
+### –- specify queue --
+#BSUB -q gpua40
+### -- set the job Name --
+#BSUB -J train_model
+### -- ask for number of cores (default: 1) --
+#BSUB -n 2
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 1:00
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=10GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u your_email_address
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
+
+nvidia-smi
+# Load the cuda module
+module load cuda/11.6
+
+/work3/s220355/diffwave_new/venv_2/bin/python3 -m diffwave /work3/s220355/diffwave_new/diffwave ~/Desktop/extracted_files/wavs
+
+/appl/cuda/11.6.0/samples/bin/x86_64/linux/release/deviceQuery
+
